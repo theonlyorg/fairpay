2c3bf20586bc43279e698be626cb1668dd937d2e
Ronelle Caguioa <ronelle.caguioa@gmail.com>
2020-06-11 12:59:07 -0700

    Create fake data context

---
+++ client/components/contexts/dataContext.js
@@ -0,0 +1,31 @@
+import React, { useState, createContext } from 'react';
+
+export const DataContext = createContext();
+
+const DataContextProvider = props => {
+  const [data, setData] = useState({
+    race: [
+      { category: 'White', percentage: 35 },
+      { category: 'Black', percentage: 30 },
+      { category: 'Hispanic', percentage: 20 },
+      { category: 'Asian', percentage: 15 },
+    ],
+    gender: [
+      { category: 'Male', percentage: 55 },
+      { category: 'Female', percentage: 45 },
+    ],
+    age: [{}],
+  });
+
+  // We would make a fetch request to the backend to retrieve the actual numbers
+  const fetchData = () => {
+    // some fetch request
+    // setData
+  }
+
+  return (
+    <DataContextProvider value={{ data, fetchData }}>{props.children}</DataContextProvider>
+  );
+};
+
+export default DataContextProvider;
