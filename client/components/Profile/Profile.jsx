118c493a2c6da06ebeb1b0105c6aded1bd387c13
Katty <kpolyak1@binghamton.edu>
2020-06-11 10:52:07 -0700

    merge with upstream master

---
+++ client/components/Profile/Profile.jsx
@@ -0,0 +1,32 @@
+/**
+ * @module Profile
+ * @author Katty Polyak
+ * @date Wed, June 10, 2020
+ * @desc Presentation component that renders a given user's profile information
+ */
+
+// import dependencies
+import React, { Component } from 'react';
+
+// import custom React Components
+import ProfileCard from './ProfileCard.jsx';
+
+// import Material UI React Components
+import { Container } from '@material-ui/core';
+
+const Profile = props => {
+  // render a React Fragment with profile cards in it
+  const profileCards = [];
+  for (let i = 0; i < 5; i++) {
+    profileCards.push(<ProfileCard />);
+  }
+  return (
+    <Container id="profile-container" maxWidth="sm">
+      <h1>Hello, world!</h1>
+      <ProfileCard type="name" edit="false" />
+      {profileCards}
+    </Container>
+  );
+};
+
+export default Profile;
---
+++ client/components/Profile/ProfileCard.jsx
@@ -0,0 +1,23 @@
+/**
+ * @module Profile
+ * @author Katty Polyak
+ * @date Wed, June 10, 2020
+ * @desc Presentation component that renders a card with a label, piece of user data, and a clickable pencil icon to edit their information.
+ * @example Name: Katty <Icon to update>
+ */
+
+import React, { Component } from 'react';
+
+import { Card, TextField } from '@material-ui/core';
+
+// editing birthdate component should be a "picker"
+
+function ProfileCard(props) {
+  return (
+    <React.Fragment>
+      <TextField variant="outlined" type="read-only" />
+    </React.Fragment>
+  );
+}
+
+export default ProfileCard;
